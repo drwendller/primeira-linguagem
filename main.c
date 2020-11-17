30066d22d5c0de98093e45445ab36d75d08d0855
Dr.Wendller <wendller1@gmail.com>
2020-11-17 19:43:01 +0000

    nada

--- main.c
+++ main.c
@@ -1,45 +1,48 @@
+//Single.TryParse ( v , out NA[aluno,nota]);
 #include <stdio.h> 
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
 #define BimestreAnual 4
-#define NumeroAluno 4
+#define TotalDeAlunos 4
 
 int main(void){
   
-   setlocale(LC_ALL,"portuguese");
-
-   int a,n;
-   float NA[NumeroAluno][BimestreAnual];
-   float MA[NumeroAluno];
-   char nome [100];
+   int aluno,nota;
+   float NA[TotalDeAlunos][BimestreAnual];
+   float MA[TotalDeAlunos];
+   char nome[TotalDeAlunos][100];
 
    printf("\n=========================aluno===============================\n");
-   printf("insira o nome do aluno %d:\n",a+1);
-   scanf("%s",nome);
+   for(int aluno=0;aluno<1;++aluno){
+   printf("insira o nome do aluno %d:\n",aluno+1);
+   scanf("%s",nome[TotalDeAlunos]);
    printf("insira a nota bimestral do %s :\n",nome);
+   }
  
-    for(int a=0;a<NumeroAluno;++a){
-     for(int n=0;n<BimestreAnual;++n){
-       scanf("%f",&NA[a][n]);
-       MA[a]+=NA[a][n];
+    for(int aluno=0;aluno<TotalDeAlunos-1;++aluno){
+     for(int nota=0;nota<BimestreAnual;++nota){
+       scanf("%f",&NA[aluno][nota]);
+       MA[aluno]+=NA[aluno][nota];
       } 
-       if(a<3){
-       MA[a]/=4;
-       printf("insira o nome do aluno %d:\n",a+2);
-       scanf("%s",nome);
-       printf("insira a nota bimestral do %s :\n",nome);
+      MA[aluno]/=4;
+        
+       printf("insira o nome do aluno %d:\n",aluno+2);
+       scanf("%s",*nome);
+       printf("insira a nota bimestral do %s :\n",*nome);
        }
-    }
+     
+    
       printf("===============================MEDIA===================================\n");
-        for(int a=0;a<NumeroAluno;++a){
-        printf("a media do aluno %s e %.2f \n",nome,MA[a]); 
-        if (MA[a]>=60){
-        printf("o aluno %s foi APROVADO\n",nome);
+      for(int aluno=0;aluno<TotalDeAlunos;++aluno){
+        printf("a media do aluno %s e %.2f \n",*nome,MA[aluno]); 
+        if (MA[aluno]>=60){
+        printf("o aluno %s foi APROVADO\n",*nome);
         }else{
-        printf("o aluno %s foi REPROVADO\n",nome);
+        printf("o aluno %s foi REPROVADO\n",*nome);
         }  
       }
+    
 
   return 0;
 }
